0c67f8e9550f70cebc447ddc46f14d8331e55363
Mrbroggle <40074860+Mrbroggle@users.noreply.github.com>
2022-02-21 09:43:00 +1100

    Create mine.py
    Mines a staircase

---
+++ mine.py
@@ -0,0 +1,19 @@
+agent.teleport_to_player()
+i=0
+agent.set_item(TORCH, 1, 1)
+agent.set_slot(1)
+agent_coords = agent.get_position()
+index()
+
+while True:
+    agent.destroy(FORWARD)
+    agent.move(FORWARD, 1)
+    agent.destroy(UP)
+    agent.destroy(DOWN)
+    loops.pause(30)
+    agent.move(DOWN, 1)
+    agent.collect_all()
+    i += 1
+    if i == 5:
+        agent.place(DOWN)
+        i = 0
